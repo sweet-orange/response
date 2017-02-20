6ce79fa19bc386a8844b15543b211793aea75ff2
z-juan <yzhouchanjuan163@163.com>
2017-02-20 09:28:35 +0800

    first respond

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/2/18.
+ */
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+
+
+    });
+})
